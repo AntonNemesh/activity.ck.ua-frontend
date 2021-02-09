002735e640c7a54a1895c2158c6ad930930c0e69
antonnemesh <anton.nemesh@gmail.com>
2021-02-09 23:18:09 +0200

    tk001: random category bugfix

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -8,16 +8,14 @@ import { CATEGORIES } from '../static/data';
 })
 export class CategoriesService {
   private categories: ICategories[] = CATEGORIES;
-  private catID: string[] = ['children', 'recreation', 'extreme', 'todo_something', 'culture',  'history',
-    'unique_things',  'sleeping',  'inspired_city', 'gastronomic_adventures'];
 
-  private getRandomNumbInRange(min, max): number {
+  static getRandomNumbInRange(min, max): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   public getRandomCategory(): string {
-    const randomNumb = this.getRandomNumbInRange(0, this.catID.length);
-    return this.catID[randomNumb];
+    const randomNumb = CategoriesService.getRandomNumbInRange(0, this.categories.length - 1);
+    return this.categories[randomNumb].id;
   }
 
   public getAllCategories(): ICategories[] {
