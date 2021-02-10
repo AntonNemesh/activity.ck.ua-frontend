cb152f2ab706afcc6f500d98bcc881d705e1580e
antonnemesh <anton.nemesh@gmail.com>
2021-02-10 12:34:22 +0200

    tk001: api-url.service implement

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,12 +1,15 @@
 import { CategoriesService } from './categories.service';
 import { PlacesService } from './places.service';
+import { ApiUrlService } from './api-url.service';
 
 export const services = [
   CategoriesService,
   PlacesService,
+  ApiUrlService,
 ];
 
 export * from './categories.service';
 export * from './places.service';
+export * from './api-url.service';
 
 
--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiUrlService } from './api-url.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private apiUrlService: ApiUrlService) { }
 
   public getPlaces(category: string, page: number, perPage: number): Observable<any> {
     const params = {
@@ -16,6 +17,6 @@ export class PlacesService {
       _limit: String(perPage),
     };
 
-    return this.http.get('http://localhost:3000/places', { params });
+    return this.http.get(this.apiUrlService.generateApiLink('places'), { params });
   }
 }
