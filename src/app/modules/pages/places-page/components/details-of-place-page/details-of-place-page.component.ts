9682c1d884fdcc57189e2a521045856ae3ba7a60
Anna Vrana <anna.o.vrana@gmail.com>
2021-02-11 10:41:20 +0200

    tk-001: fix details-place-page

--- src/app/modules/pages/places-page/components/details-of-place-page/details-of-place-page.component.ts
+++ src/app/modules/pages/places-page/components/details-of-place-page/details-of-place-page.component.ts
@@ -11,6 +11,7 @@ import {IDetailsOfPlaceInterface} from '../../../../../static/type';
 export class DetailsOfPlacePageComponent implements OnInit {
   public placeId: string;
   public nameOfPlace: string;
+  public detailOfPlace: IDetailsOfPlaceInterface;
   public detailsPlaceId: number;
   public detailsCategoryId: string;
   public detailsPhotos: Array<string>;
@@ -35,18 +36,17 @@ export class DetailsOfPlacePageComponent implements OnInit {
     });
     this.detailsOfPlaces.getDetailsOfPlace(this.placeId).subscribe(
       (detailsOfPlace: IDetailsOfPlaceInterface) => {
-        this.detailsPhotos = detailsOfPlace.photos;
-        this.nameOfPlace = detailsOfPlace.name;
-        this.detailsPhone = detailsOfPlace.phone;
-        this.detailAddress = detailsOfPlace.address;
-        this.detailsWebSite = detailsOfPlace.webSite;
-        this.detailsWorkTime = detailsOfPlace.workTime;
-        this.detailsAccessibility = detailsOfPlace.accessibility;
-        this.detailsDogFriendly = detailsOfPlace.dogFriendly;
-        this.detailsChildFriendly = detailsOfPlace.childFriendly;
-        this.detailsAboutInfo = detailsOfPlace.aboutInfo;
-        this.detailsRating = detailsOfPlace.rating;
+        this.detailsPhotos = detailsOfPlace[0][`photos`];
+        this.nameOfPlace = detailsOfPlace[0][`name`];
+        this.detailsPhone = detailsOfPlace[0][`phone`];
+        this.detailAddress = detailsOfPlace[0][`address`];
+        this.detailsWebSite = detailsOfPlace[0][`webSite`];
+        this.detailsWorkTime = detailsOfPlace[0][`workTime`];
+        this.detailsAccessibility = detailsOfPlace[0][`accessibility`];
+        this.detailsDogFriendly = detailsOfPlace[0][`dogFriendly`];
+        this.detailsChildFriendly = detailsOfPlace[0][`childFriendly`];
+        this.detailsAboutInfo = detailsOfPlace[0][`aboutInfo`];
+        this.detailsRating = detailsOfPlace[0][`rating`];
       });
-    console.log('this.this.placeId', this.nameOfPlace);
   }
 }
