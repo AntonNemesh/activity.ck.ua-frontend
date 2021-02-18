3dee40ca1c8f05b56149805868c9614af863f63b
antonnemesh <anton.nemesh@gmail.com>
2021-02-18 23:25:47 +0200

    tk001.1: pagination dynamic buttons

--- src/app/modules/shared/components/pagination/pagination.component.ts
+++ src/app/modules/shared/components/pagination/pagination.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {PlacesService} from '../../../../services';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PlacesService } from '../../../../services';
 
 @Component({
   selector: 'app-pagination',
@@ -11,9 +11,12 @@ export class PaginationComponent implements OnInit {
   constructor(private placesService: PlacesService) { }
 
   private typesId;
-  private page;
+  private page: number;
+  private buttonStart: number;
+  private buttonEnd: number;
 
-  public totalPages;
+  public totalPages: number;
+  public separatorState: boolean[];
 
   @Input()
   categoryId;
@@ -35,10 +38,46 @@ export class PaginationComponent implements OnInit {
     this.page = 1;
   }
 
+  private initSeparator(): void {
+    this.separatorState = [false, false];
+  }
+
+  private setVisibleButtons(buttonStart, buttonEnd, [separator1, separator2]): void {
+    this.buttonStart = buttonStart;
+    this.buttonEnd = buttonEnd;
+    this.separatorState = [separator1, separator2];
+  }
+
+  private updateButtonsView(): void {
+    if (this.totalPages <= 7) {
+      this.setVisibleButtons(1, this.totalPages, [false, false]);
+      return;
+    }
+    if (this.page < 6) {
+      this.setVisibleButtons(1, 6, [false, true]);
+      return;
+    }
+    if (this.page > (this.totalPages - 5) ) {
+      this.setVisibleButtons(this.totalPages - 5, this.totalPages, [true, false]);
+      return;
+    }
+    this.setVisibleButtons(this.page - 2, this.page + 2, [true, true]);
+  }
+
+  public checkButtonVisibility(page): boolean {
+    let result = true;
+    if (page === 1 ||
+        page === this.totalPages ||
+        page < this.buttonStart ||
+        page > this.buttonEnd) { result = false; }
+    return result;
+  }
+
   public setPage(event, page): void {
     event.preventDefault();
     this.page = page;
     this.paginationStateChange.emit(this.page);
+    this.updateButtonsView();
   }
 
   public checkState(page): boolean {
@@ -51,11 +90,14 @@ export class PaginationComponent implements OnInit {
     this.typesId = value;
     this.updateTotalPages();
     this.resetPage();
+    this.updateButtonsView();
   }
 
   ngOnInit(): void {
     this.updateTotalPages();
     this.resetPage();
+    this.initSeparator();
+    this.updateButtonsView();
   }
 
 }
