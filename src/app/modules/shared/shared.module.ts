ae7a614048339e6058a0ac977b21110110a947b8
Anna Vrana <anna.o.vrana@gmail.com>
2021-02-26 19:05:17 +0200

    001.3: created gallery component in shared module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent, LoadingComponent } from './components';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 
 @NgModule({
-  declarations: [LoadingComponent, PaginationComponent],
+  declarations: [
+    LoadingComponent,
+    PaginationComponent,
+    GalleryComponent,
+  ],
   exports: [
     LoadingComponent,
     PaginationComponent,
+    GalleryComponent,
   ],
   imports: [
     CommonModule,
