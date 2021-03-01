7fc508e545370acb2963c9f9d97a835a8fbad2df
Anna Vrana <anna.o.vrana@gmail.com>
2021-03-01 06:24:58 +0200

    001.3: getCurrentPhoto by dots

--- src/app/modules/shared/components/gallery/gallery.component.ts
+++ src/app/modules/shared/components/gallery/gallery.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IPhotos } from '../../../../static/type';
-import { PlacesService } from '../../../../services';
+import {Component, Input, OnInit} from '@angular/core';
+import {IPhotos} from '../../../../static/type';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -8,12 +8,40 @@ import { PlacesService } from '../../../../services';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-  constructor(private placesService: PlacesService) { }
+  public totalImages: number;
+  public selectedDot: number;
+  public selectedPhoto: IPhotos;
+  public interval: any = interval(5000);
+
+  constructor() {
+  }
 
   @Input()
   photos: IPhotos[];
 
-  ngOnInit(): void {
-    console.log(this.photos.length);
+  public getPhoto(index: number): void {
+    this.selectedPhoto = this.photos[index];
   }
+
+  public getCurrentPhoto(event: Event, index: number): void {
+    this.selectedDot = index;
+    this.getPhoto(this.selectedDot);
+  }
+
+  public getNextPhoto(): void {
+
+  }
+
+  public rotatePhotos(index: number): void {
+    this.selectedPhoto = this.photos[index];
+    this.photos.forEach(item => {
+      this.interval.subscribe(this.selectedPhoto = item, console.log('interval', this.selectedPhoto));
+    });
+  }
+
+  ngOnInit(): void {
+    this.totalImages = this.photos.length;
+    this.rotatePhotos(0);
+  }
+
 }
