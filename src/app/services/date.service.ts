dcba206a1283ff164c177a03df0aa1b28492db0e
antonnemesh <anton.nemesh@gmail.com>
2021-04-07 17:03:38 +0300

    001.6: improved getWorkState method in date service

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -21,17 +21,29 @@ export class DateService {
   public getWorkState(workTime: IWorkTime): string {
     const date: Date = new Date();
     let weekday: string = this.getShortNameOfMonth(date);
-    let condition: boolean = false;
+    let conditionStartDay: boolean = false;
+    let conditionEndDay: boolean = false;
 
     if (workTime[weekday]) {
+      const startWorkDate: Date = new Date(
+        date.getFullYear(), date.getMonth(), date.getDate(),
+        workTime[weekday].start.split(':')[0], workTime[weekday].start.split(':')[1]
+      );
       const endWorkDate: Date = new Date(
         date.getFullYear(), date.getMonth(), date.getDate(),
         workTime[weekday].end.split(':')[0], workTime[weekday].end.split(':')[1]
       );
-      condition = (+endWorkDate - +date) > 0;
+      conditionStartDay = (+startWorkDate - +date) < 0;
+      conditionEndDay = (+endWorkDate - +date) > 0;
     }
 
-    if (condition) { return `Відчинено ${String.fromCharCode(0x0387)} Зачиняється: ${workTime[weekday].end}`; }
+    if (conditionStartDay && conditionEndDay) {
+      return `Відчинено ${String.fromCharCode(0x0387)} Зачиняється: ${workTime[weekday].end}`;
+    }
+
+    if (!conditionStartDay && conditionEndDay) {
+      return `Зачинено ${String.fromCharCode(0x0387)} Відчиняється: ${workTime[weekday].start}`;
+    }
 
     do {
       date.setDate(date.getDate() + 1);
