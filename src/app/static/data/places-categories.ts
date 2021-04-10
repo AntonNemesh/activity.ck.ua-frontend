ea2906a7a1fe78cc40c2ac5881e5518a0673a240
Anna1o1Vrana <anna.o.vrana@gmail.com>
2021-04-10 22:02:08 +0300

    001: renamed categories and types

--- src/app/static/data/places-categories.ts
+++ src/app/static/data/places-categories.ts
@@ -1,14 +1,10 @@
 import { IPlacesCategories } from '../type';
 
 export const PLACES_CATEGORIES: IPlacesCategories[] = [
-  {id: 'children', name: 'розваги з дітьми'},
-  {id: 'recreation', name: 'активний відпочинок'},
-  {id: 'extreme', name: 'гострі відчуття'},
-  {id: 'todo_something', name: 'зробити руками'},
-  {id: 'culture', name: 'культурне натхнення'},
-  {id: 'history', name: 'історичні враження'},
-  {id: 'unique_things', name: 'унікальні речі'},
   {id: 'sleeping', name: 'затишну ночівлю'},
-  {id: 'inspired_city', name: 'натхнення містом'},
-  {id: 'gastronomic_adventures', name: 'гастрономічні пригоди'},
+  {id: 'gastronomy', name: 'гастрономічні пригоди'},
+  {id: 'recreation', name: 'місце відпочинку'},
+  {id: 'culture', name: 'культурне натхнення'},
+  {id: 'unique', name: 'унікальні речі'},
+  {id: 'navigation', name: 'навігацію містом'},
 ];
