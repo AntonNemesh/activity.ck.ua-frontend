1eac45d742ee8edf1e3fb2df55dd17a31668939d
antonnemesh <anton.nemesh@gmail.com>
2021-04-14 21:22:12 +0300

    001.12: favorite and visit functional for places

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -12,12 +12,12 @@ export class UsersService {
 
   constructor(private http: HttpClient, private apiUrlService: ApiUrlService) { }
 
-  getUserInfo(): Observable<IUserResponse> {
+  public getUserInfo(): Observable<IUserResponse> {
     return this.http.get<IUserResponse>(this.apiUrlService.generateApiLink('users/myself'));
     // return this.http.get<IUser>('http://localhost:3001/users-myself');
   }
 
-  getVisitedPlaces(page: number, limit: number): Observable<IPlacesResponse> {
+  public getVisitedPlaces(page: number, limit: number): Observable<IPlacesResponse> {
     let params: HttpParams = new HttpParams();
     params = params.set('_page', page.toString());
     params = params.set('_limit', limit.toString());
@@ -26,7 +26,7 @@ export class UsersService {
     // return this.http.get<IObjectActivity[]>('http://localhost:3001/users-myself-visited_places', { params });
   }
 
-  getFavoritePlaces(page: number, limit: number): Observable<IPlacesResponse> {
+  public getFavoritePlaces(page: number, limit: number): Observable<IPlacesResponse> {
     let params: HttpParams = new HttpParams();
     params = params.set('_page', page.toString());
     params = params.set('_limit', limit.toString());
@@ -35,7 +35,7 @@ export class UsersService {
     // return this.http.get<IObjectActivity[]>('http://localhost:3001/users-myself-favorite_places', { params });
   }
 
-  getCreatedPlaces(page: number, limit: number): Observable<IPlacesResponse> {
+  public getCreatedPlaces(page: number, limit: number): Observable<IPlacesResponse> {
     let params: HttpParams = new HttpParams();
     params = params.set('_page', page.toString());
     params = params.set('_limit', limit.toString());
@@ -44,7 +44,7 @@ export class UsersService {
     // return this.http.get<IObjectActivity[]>('http://localhost:3001/users-myself-created_places', { params });
   }
 
-  getCreatedEvents(page: number, limit: number): Observable<IEventsResponse> {
+  public getCreatedEvents(page: number, limit: number): Observable<IEventsResponse> {
     let params: HttpParams = new HttpParams();
     params = params.set('_page', page.toString());
     params = params.set('_limit', limit.toString());
@@ -53,12 +53,28 @@ export class UsersService {
     // return this.http.get<IObjectActivity[]>('http://localhost:3001/users-myself-created_events', { params });
   }
 
-  getScheduledEvents(page: number, limit: number): Observable<IEventsResponse> {
+  public getScheduledEvents(page: number, limit: number): Observable<IEventsResponse> {
     let params: HttpParams = new HttpParams();
     params = params.set('_page', page.toString());
     params = params.set('_limit', limit.toString());
 
     return this.http.get<IEventsResponse>(this.apiUrlService.generateApiLink('users/myself/scheduled_events'), { params });
     // return this.http.get<IObjectActivity[]>('http://localhost:3001/users-myself-scheduled_events', { params });
+  }
+
+  public addPlaceToFavorite(placeId: string): Observable<any> {
+    return this.http.post<any>(this.apiUrlService.generateApiLink(`users/myself/favorite_places/${placeId}`), null);
+  }
+
+  public removePlaceFromFavorite(placeId: string): Observable<any> {
+    return this.http.delete<any>(this.apiUrlService.generateApiLink(`users/myself/favorite_places/${placeId}`));
+  }
+
+  public addPlaceToVisited(placeId: string): Observable<any> {
+    return this.http.post<any>(this.apiUrlService.generateApiLink(`users/myself/visited_places/${placeId}`), null);
+  }
+
+  public removePlaceFromVisited(placeId: string): Observable<any> {
+    return this.http.delete<any>(this.apiUrlService.generateApiLink(`users/myself/visited_places/${placeId}`));
   }
 }
