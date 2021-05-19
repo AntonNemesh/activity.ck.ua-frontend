8ac5e3b6032699293c33292190108628d8b4e78c
antonnemesh <anton.nemesh@gmail.com>
2021-05-19 16:41:49 +0300

    DEV: changed backend API for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { IEnvironment } from '../app/static/type';
 
 export const environment: IEnvironment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/v1'
+  // apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'https://activity-ck.herokuapp.com:443/api/v1'
 };
